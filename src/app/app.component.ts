ed854aa3ceaf204e81aa14b2b3336a845ce8718a
kylon-tyner <156705815+kylon-tyner@users.noreply.github.com>
2024-01-16 11:00:11 -0500

    Added click event to single button

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 
+
+
 @Component({
   selector: 'app-root',
   standalone: true,
@@ -11,4 +13,17 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'google-analytics';
+
+  sendEvent(clickEvent: MouseEvent) {
+    window.dataLayer.push({
+      event: 'button_click',
+      origin: 'app.component',
+      page_title: 'App Component',
+      logged_in: false,
+      user_type: 'admin',
+      contact_id: '123456',
+      event_action: 'N/A',
+      event_label: 'Send Click Event'
+    });
+  }
 }
--- src/main.ts
+++ src/main.ts
@@ -2,5 +2,14 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
 
+
+declare global {
+  interface Window {
+    dataLayer: any[];
+  }
+}
+
+window.dataLayer = window.dataLayer || [];
+
 bootstrapApplication(AppComponent, appConfig)
   .catch((err) => console.error(err));
